c187e9ebef3830e9222b9ffe8b2f0b7ae32107b8
tragshy <trag352k2@gmail.com>
2021-10-11 09:38:22 +0700

    BTCTTDL_GT

---
+++ Tuan01/lab1bai2.java
@@ -0,0 +1,20 @@
+public class lab1bai2
+{   
+    public static void main(String[] args) 
+    {  
+        Scanner in = new Scanner(System.in);
+        System.out.println("Nhap chieu dai ");
+        int dai = in.nextInt();
+        System.out.println("Nhap chieu rong ");
+        int rong = in.nextInt();
+        int min = Math.min(dai, rong);
+        double chuvi = (dai+rong)*2;
+        double s= dai*rong;
+        System.out.println("Chu vi = "+chuvi+" Dien tich = "+s+ " Canh nho nhat "+min);
+
+
+    
+    }
+    
+    
+}
---
+++ Tuan03/lab3bai1.java
@@ -0,0 +1,18 @@
+public class lab3bai1 {
+    public static void main(String[] args) {
+        System.out.println("Nhập vào số nguyên n: ");
+        Scanner scanner = new Scanner(System.in);
+        int son = scanner.nextInt();
+        boolean kiemTra = true;
+        for(int i =2; i < son; i++){
+            if(son % i == 0){ 
+            kiemTra = false;
+            break;
+            }
+        }
+        if(kiemTra == true)
+            System.out.printf("%d là số nguyên tố",son);
+        else
+            System.out.printf("%d không phải là số nguyên tố",son);
+    }
+}
